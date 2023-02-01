7e5fd2856bd43133cf1db5a8cd72289f6d95951b
Aminat Okunuga <makadeaminat@gmail.com>
2023-02-01 23:44:20 +0100

    Add Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function addCategory(Request $request){
-        // if($request->isMethod('post')){
-        //     $data = $request->all();
-            
-        // }
+        if($request->isMethod('post')){
+            $data = $request->all();
+            $category = new Category;
+            $category->name = $data['category_name'];
+            $category->description = $data['description'];
+            $category->url = $data['url'];
+            $category->save();
+        }
         return view('admin.categories.add_category');
     }
 }
