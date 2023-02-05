ab2fc295796a08f01a5ebe8b8500cf0f6699c8ff
Aminat Okunuga <makadeaminat@gmail.com>
2023-02-05 02:45:10 +0100

    Confirm delete

--- public/js/backend_js/matrix.form_validation.js
+++ public/js/backend_js/matrix.form_validation.js
@@ -157,4 +157,12 @@ $(document).ready(function(){
 			$(element).parents('.control-group').addClass('success');
 		}
 	});
+
+	$("#delCat").click(function(){
+		alert("test");
+		if(confirm('Are you sure you want to delete this Category?')){
+			return true;
+		}
+		return false;
+	});
 });
--- resources/views/admin/categories/view_categories.blade.php
+++ resources/views/admin/categories/view_categories.blade.php
@@ -44,7 +44,7 @@
                   <td>{{$category->url}}</td>
                   <td class="center">
                     <a href="{{url('/admin/edit-category/'.$category->id)}}" class="btn btn-primary btn-mini">Edit</a>
-                    <a href="{{url('/admin/delete-category/'.$category->id)}}" class="btn btn-danger btn-mini">Delete</a>
+                    <a id="delCat" href="{{url('/admin/delete-category/'.$category->id)}}" class="btn btn-danger btn-mini">Delete</a>
                   </td>
                 </tr>
                 @endforeach
